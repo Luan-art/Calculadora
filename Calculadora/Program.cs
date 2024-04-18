ebd06824844b43633caf143e6f1fe9da91d6281a
Luan-art <luanlf2003@gmail.com>
2024-04-18 18:37:11 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+
+float numero1, numero2;
+
+Console.WriteLine("Fala um numero: ");
+
+numero1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Fala otro: ");
+
+numero2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"A Soma é: {numero1 + numero2} ");
+Console.WriteLine($"A Subtração é:  {numero1 - numero2}");
+Console.WriteLine($"A multiplicação é:  {numero1 * numero2}");
+
+if (numero2 != 0 || numero2 > 0)
+{
+
+    Console.WriteLine($"A divisão é:  {numero1 / numero2}");
+
+}
+else
+{
+    Console.WriteLine("DIVIDINDO POR 0!!!!!!!!!!!!!!????? :<");
+}
+
