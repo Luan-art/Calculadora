fbcdd6c6cd5d0acb2910d92ae62c39d0208a0d96
Luan-art <luanlf2003@gmail.com>
2024-04-18 19:09:35 -0300

    Mudanças Iniciais

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,27 +1,59 @@
 ﻿using System.Reflection.PortableExecutable;
 
-float numero1, numero2;
+float numero1 = 0, numero2 = 1;
 
-Console.WriteLine("Fala um numero: ");
+int escolha = 0;
 
-numero1 = float.Parse(Console.ReadLine());
-
-Console.WriteLine("Fala otro: ");
-
-numero2 = float.Parse(Console.ReadLine());
-
-Console.WriteLine($"A Soma é: {numero1 + numero2} ");
-Console.WriteLine($"A Subtração é:  {numero1 - numero2}");
-Console.WriteLine($"A multiplicação é:  {numero1 * numero2}");
-
-if (numero2 != 0 || numero2 > 0)
+do
 {
 
-    Console.WriteLine($"A divisão é:  {numero1 / numero2}");
+    Console.WriteLine("Fale um numero: ");
 
-}
-else
-{
-    Console.WriteLine("DIVIDINDO POR 0!!!!!!!!!!!!!!????? :<");
-}
+    numero1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Fale outro numero: ");
+
+    numero2 = float.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Escolha uma Operação\n" +
+        "1 = soma\n" +
+        "2 = subtração\n" +
+        "3 = multiplicação\n" +
+        "4 = divisão\n" +
+        "0 = finalizar\n ");
+
+    escolha = int.Parse(Console.ReadLine());
+
+
+    if (escolha == 1)
+    {
+        Console.WriteLine($"A Soma é: {numero1 + numero2} ");
+    
+    }
+    else if (escolha == 2)
+    {
+        Console.WriteLine($"A Subtração é:  {numero1 - numero2}");
+    }
+    else if (escolha == 3)
+    {
+        Console.WriteLine($"A multiplicação é:  {numero1 * numero2}");
+    }
+    else if(escolha == 4 && numero2 != 0)
+    {
+        Console.WriteLine($"A divisão é:  {numero1 / numero2}");
+
+
+    }
+    else if (numero2 == 0)
+    {
+        Console.WriteLine("Não se divide por 0");
+    }
+
+
+
+} while (escolha != 0);
+
+
+
 
