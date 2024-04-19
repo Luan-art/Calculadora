cca6a3a497a58d4fcd051c3434184bfb8d2d9aea
Luan-art <luanlf2003@gmail.com>
2024-04-19 10:19:49 -0300

    quatro numero

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System.Reflection.PortableExecutable;
 
-float numero1 = 0, numero2 = 1;
+float numero1 = 0, numero2 = 0, numero3= 0, numero4 = 0;
 
 int escolha = 0;
 
 do
 {
-
     Console.WriteLine("Fale um numero: ");
-
     numero1 = float.Parse(Console.ReadLine());
 
-    Console.WriteLine("Fale outro numero: ");
-
+    Console.WriteLine("\nFale outro numero: ");
     numero2 = float.Parse(Console.ReadLine());
 
+
+    Console.WriteLine("\nFale outro numero: ");
+    numero3 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("\nFale o último numero: ");
+    numero4 = float.Parse(Console.ReadLine());
 
     Console.WriteLine("Escolha uma Operação\n" +
         "1 = soma\n" +
@@ -22,35 +25,23 @@ do
         "3 = multiplicação\n" +
         "4 = divisão\n" +
         "0 = finalizar\n ");
-
     escolha = int.Parse(Console.ReadLine());
 
-
-    if (escolha == 1)
-    {
-        Console.WriteLine($"A Soma é: {numero1 + numero2} ");
-    
+    if (escolha == 1){
+        Console.WriteLine($"\nA Soma é: {numero1 + numero2 + numero3 + numero4} ");  
     }
-    else if (escolha == 2)
-    {
-        Console.WriteLine($"A Subtração é:  {numero1 - numero2}");
+    else if (escolha == 2){
+        Console.WriteLine($"\nA Subtração é:  {numero1 - numero2 - numero3 - numero4}");
     }
-    else if (escolha == 3)
-    {
-        Console.WriteLine($"A multiplicação é:  {numero1 * numero2}");
+    else if (escolha == 3){
+        Console.WriteLine($"\nA multiplicação é:  {numero1 * numero2 * numero3 * numero4}");
     }
-    else if(escolha == 4 && numero2 != 0)
-    {
-        Console.WriteLine($"A divisão é:  {numero1 / numero2}");
-
-
+    else if(escolha == 4 && numero2 != 0 && numero3 != 0 && numero4 !=0){
+        Console.WriteLine($"\nA divisão é:  {((numero1 / numero2)/numero3)/numero4}");
     }
-    else if (numero2 == 0)
-    {
-        Console.WriteLine("Não se divide por 0");
+    else if (numero2 == 0 || numero3 == 0 || numero4 == 0){
+        Console.WriteLine("\nNão se divide por 0");
     }
-
-
 
 } while (escolha != 0);
 
