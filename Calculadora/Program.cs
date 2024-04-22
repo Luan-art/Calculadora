3a3b5374429b70eb718ec183bd7d021ac4f30ab9
Luan-art <luanlf2003@gmail.com>
2024-04-22 11:59:01 -0300

    swichCase

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.PortableExecutable;
+﻿using System.Linq.Expressions;
+using System.Reflection.PortableExecutable;
 
 float numero1 = 0, numero2 = 0, numero3= 0, numero4 = 0;
 
@@ -9,17 +10,25 @@ do
     Console.WriteLine("Fale um numero: ");
     numero1 = float.Parse(Console.ReadLine());
 
-    Console.WriteLine("\nFale outro numero: ");
-    numero2 = float.Parse(Console.ReadLine());
+    do{
+        Console.WriteLine("\nFale outro numero: ");
+        numero2 = float.Parse(Console.ReadLine());
+    } while (numero2 == 0);
+
+    do
+    {
+        Console.WriteLine("\nFale outro numero: ");
+        numero3 = float.Parse(Console.ReadLine());
+    } while (numero3 == 0);
 
 
-    Console.WriteLine("\nFale outro numero: ");
-    numero3 = float.Parse(Console.ReadLine());
+    do { 
+        Console.WriteLine("\nFale o último numero: ");
+        numero4 = float.Parse(Console.ReadLine());
+    } while (numero4 == 0);
 
-    Console.WriteLine("\nFale o último numero: ");
-    numero4 = float.Parse(Console.ReadLine());
 
-    Console.WriteLine("Escolha uma Operação\n" +
+Console.WriteLine("Escolha uma Operação\n" +
         "1 = soma\n" +
         "2 = subtração\n" +
         "3 = multiplicação\n" +
@@ -27,20 +36,30 @@ do
         "0 = finalizar\n ");
     escolha = int.Parse(Console.ReadLine());
 
-    if (escolha == 1){
-        Console.WriteLine($"\nA Soma é: {numero1 + numero2 + numero3 + numero4} ");  
-    }
-    else if (escolha == 2){
-        Console.WriteLine($"\nA Subtração é:  {numero1 - numero2 - numero3 - numero4}");
-    }
-    else if (escolha == 3){
-        Console.WriteLine($"\nA multiplicação é:  {numero1 * numero2 * numero3 * numero4}");
-    }
-    else if(escolha == 4 && numero2 != 0 && numero3 != 0 && numero4 !=0){
-        Console.WriteLine($"\nA divisão é:  {((numero1 / numero2)/numero3)/numero4}");
-    }
-    else if (numero2 == 0 || numero3 == 0 || numero4 == 0){
-        Console.WriteLine("\nNão se divide por 0");
+    
+    
+    switch (escolha)
+    {
+        case 1:
+            Console.WriteLine($"\nA Soma é: {numero1 + numero2 + numero3 + numero4} ");
+            break;
+
+        case 2:
+            Console.WriteLine($"\nA Subtração é:  {numero1 - numero2 - numero3 - numero4}");
+            break;
+
+        case 3:
+            Console.WriteLine($"\nA multiplicação é:  {numero1 * numero2 * numero3 * numero4}");
+            break;
+
+        case 4:
+            Console.WriteLine($"\nA divisão é:  {((numero1 / numero2) / numero3) / numero4}");
+            break;
+
+        default:
+            Console.WriteLine("Nenhuma conta selecionada");
+            break;
+
     }
 
 } while (escolha != 0);
